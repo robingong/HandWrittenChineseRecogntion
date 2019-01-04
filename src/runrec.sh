93339f049d8f6c89c24011fabf77841a8926b743
alex <a@slife.io>
2019-01-04 10:16:34 +0800

    delete useless lines

--- src/runrec.sh
+++ src/runrec.sh
@@ -1,20 +1,6 @@
+#!/usr/bin/env bash
 python chinese_rec.py --mode=train --max_steps=200000 --eval_steps=1000 --save_steps=10000
 # python chinese_rec.py --mode=validation
 
-# python chinese_rec.py --mode=validation
-
-# python chinese_rec.py --mode=validation
-
-# python chinese_rec.py --mode=validation
-
-# python chinese_rec.py --mode=validation
-
-# python chinese_rec.py --mode=validation
-
-# python chinese_rec.py --mode=validation
-
-# python chinese_rec.py --mode=validation
-
-# python chinese_rec.py --mode=validation
 
 
